6a84f25b4d0a5b7c93076076683b1e9e8d3fe157
Tinghui <tinghui.zhang3@gmail.com>
2015-06-10 11:15:29 +0800

    add some functions

--- FoundationExtensions/NSObjCRuntime+Extensions.h
+++ FoundationExtensions/NSObjCRuntime+Extensions.h
@@ -36,8 +36,10 @@
 #pragma mark - Math
 //#define fequal(a, b)            (fabs((a) - (b)) < FLT_EPSILON)
 //#define fequalzero(a)           (fabs(a) < FLT_EPSILON)
-#define DegreesToRadian(x)      (M_PI * (x) / 180.0f)
-#define RadianToDegrees(x)      (180.0f * (x) / M_PI)
-#define RandomNumber(min, max)  ((min) + arc4random()%((max)-(min)+1))
+#define DegreesToRadian(x)          (M_PI*(x)/180.0f)
+#define RadianToDegrees(x)          (180.0f*(x)/M_PI)
+#define RandomNumber(min, max)      ((min)+arc4random()%((max)-(min)+1))
+#define RandomFloatNumber(min, max) ((((float)(arc4random()%((unsigned)RAND_MAX+1))/RAND_MAX)*((max)-(min)))\
+                                        +(min))
 
 #endif
